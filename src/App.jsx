aa2796b730730129157663bf1e1fbf66ef4d7de4
bherth <bherthchavez@gmail.com>
2023-09-27 23:47:04 +0800

    meta2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import Head from "./components/Head"
 import Navbar from "./components/Navbar"
 import { Analytics } from '@vercel/analytics/react';
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import pjLogo from '../src/assets/pjlogo.svg'
+// import pjLogo from '../src/assets/pjlogo.svg'
+// import { Helmet, HelmetProvider } from "react-helmet-async";
 function App() {
 
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
+      {/* <HelmetProvider> */}
+        {/* <Helmet> */}
           {/* <title>Paula & Julbert</title> */}
-          <meta property="og:image" content={pjLogo}/>
-        </Helmet>
+          {/* <meta property="og:image" content={pjLogo}/> */}
+        {/* </Helmet> */}
         <main className="bg-[#fef1e2] h-screen">
           <Navbar />
           <Head />
           <Analytics />
         </main>
-      </HelmetProvider>
+      {/* </HelmetProvider> */}
     </>
   )
 }
