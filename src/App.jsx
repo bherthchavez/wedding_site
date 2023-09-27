eb51d3f45dae6c46bb40a7421db5e0b59c404ffd
bherth <bherthchavez@gmail.com>
2023-09-28 00:42:12 +0800

    mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import Head from "./components/Head"
 import Navbar from "./components/Navbar"
 import { Analytics } from '@vercel/analytics/react';
-// import pjLogo from '../src/assets/pjlogo.svg'
-// import { Helmet, HelmetProvider } from "react-helmet-async";
 function App() {
 
   return (
     <>
-      {/* <HelmetProvider> */}
-        {/* <Helmet> */}
-          {/* <title>Paula & Julbert</title> */}
-          {/* <meta property="og:image" content={pjLogo}/> */}
-        {/* </Helmet> */}
-        <main className="bg-[#fef1e2] h-screen">
-          <Navbar />
-          <Head />
-          <Analytics />
-        </main>
-      {/* </HelmetProvider> */}
+      <main className="bg-[#fef1e2] h-screen">
+        <Navbar />
+        <Head />
+        <Analytics />
+      </main>
     </>
   )
 }
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -11,20 +11,27 @@ const Head = () => {
       <div className='fixed bottom-10 right-[-280px]'>
         <img src={pjLogo} className='w-[500px] h-[500px] opacity-5' />
       </div>
-      <div className="max-w-[800px] w-full h-auto sm:h-screen mx-auto text-left flex flex-col justify-center px-4">
+      <div className="max-w-[800px]  w-full h-auto sm:h-screen mx-auto text-left flex flex-col justify-center px-4">
 
-
-        <div className='flex justify-center items-center'>
-          <img src={pj} className='w-[200px] sm:w-[300px] h-[200px] sm:h-[300px] p-2' />
-          <h1 className='font-freestyle font-semibold text-[100px] sm:text-[180px]'>
-
-            <span className='text-[#724526] relative top-[-60px] tracking-wide'>aula</span>
-            <span className='text-[#292929] relative top-[-10px] right-[80px]  text-[60px] tracking-wide font-normal opacity-40'>and</span>
-            <span className='text-[#292929] relative top-[100px] right-[350px] tracking-wide'>ulbert</span>
-         
-          </h1>
+        {/* Web */}
+        <div className='hidden sm:flex'>
+          <img src={pj} className='w-[300px] h-[300px]  ml-24' />
+          <div className='relative font-freestyle font-semibold text-[180px]'>
+            <span className='text-[#724526] absolute top-[-45px] tracking-wide'>aula</span>
+            <span className='text-[#292929] absolute top-[130px] left-[100px]  text-[60px] tracking-wide font-normal opacity-40'>and</span>
+            <span className='text-[#292929] absolute top-[100px] left-[-100px] tracking-wide'>ulbert</span>
+          </div>
         </div>
 
+        {/* mobile */}
+        <div className='flex sm:hidden h-screen w-full items-center'>
+          <img src={pj} className='h-[200px]' />
+          <div className='relative font-freestyle font-semibold text-[100px]'>
+            <span className='text-[#724526] absolute top-[-100px] tracking-wide'>aula</span>
+            <span className='text-[#292929] absolute top-[-10px] left-[50px]  text-[50px] tracking-wide font-normal opacity-40'>and</span>
+            <span className='text-[#292929] absolute top-[-5px] left-[-70px] tracking-wide'>ulbert</span>
+          </div>
+        </div>
       </div>
     </>
   )
