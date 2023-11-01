8d1042c665895052521e0c43a3ecb2b97685a9de
bherth <bherthchavez@gmail.com>
2023-11-02 01:38:34 +0800

    done rsvp

--- src/components/Rsvp.jsx
+++ src/components/Rsvp.jsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import useApi from "../hooks/useAPI";
 import pg from '../assets/pj.png'
+import firebase from "../firebase";
+
+
 function Rsvp() {
   const params = useParams()
 
 
   const [foundInvited, setFoundInvited] = useState([])
+  const [btnStatus, setBtnStatus] = useState("")
   // eslint-disable-next-line no-unused-vars
   const [refetchTrigger, setRefetchTrigger] = useState(false);
 
@@ -17,27 +21,63 @@ function Rsvp() {
     const fetchInvited = async () => {
       try {
         const result = await getInvited()
-        setFoundInvited(result.find(inv => inv.id === params.id))
+        const existing = result.find(inv => inv.id === params.id)
+        if(existing){
+          setFoundInvited(existing)
+          setBtnStatus(existing.status)
+        }
+
       } catch (error) {
         console.log('Error fetching data:', error);
       }
     }
     fetchInvited();
+  
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [refetchTrigger])
 
   console.log(foundInvited)
+  console.log(btnStatus)
+
+
+  const attending = () => {
+    firebase
+      .firestore().collection('invited')
+      .doc(params.id)
+      .update({ status: "attending" })
+      .then(() => {
+        console.log("status updated!");
+        setRefetchTrigger(prev => !prev)
+      }).catch((error) => {
+        console.log(error.message)
+      });
+    
+  };
+  const notAttending = () => {
+    firebase
+      .firestore().collection('invited')
+      .doc(params.id)
+      .update({ status: "not attending" })
+      .then(() => {
+        console.log("status updated!");
+        setRefetchTrigger(prev => !prev)
+      }).catch((error) => {
+        console.log(error.message)
+      });
+    
+  };
+
 
   return (
     <section name='Kulay' className="flex flex-col justify-between bg-custom2 bg-local bg-center bg-cover h-screen  font-sans">
       <div className="container flex flex-col p-8 gap-5 sm:gap-10 mx-auto md:p-8 max-w-[1200px]">
         <div className='flex flex-col justify-center items-center'>
-         <div className="flex flex-col justify-center items-center text-[#745129] mb-4 text-xs gap-1 italic">
-          <a href='/' className="cursor-pointer">
-            <img src={pg} alt='Logo' className='w-20' />
-          </a>
-          <span>Disyembre  3, 2023</span>
-         </div>
+          <div className="flex flex-col justify-center items-center text-[#745129] mb-4 text-xs gap-1 italic">
+            <a href='/' className="cursor-pointer">
+              <img src={pg} alt='Logo' className='w-20' />
+            </a>
+            <span>Disyembre  3, 2023</span>
+          </div>
           {/* <div className='text-[#797979]  text-center text-lg sm:text-[25px] leading-none sm:leading-[29px] '>
             Pamantayan ng <br /> Pananamit at RSVP
           </div>
@@ -47,21 +87,36 @@ function Rsvp() {
         </div>
 
         <div className='text-[#727171] flex flex-col justify-center items-center gap-2  text-center text-[25px] sm:text-[35px] leading-7 sm:leading-[43px] '>
-          <div>Kamusta {foundInvited.first_name === 'male' ? 'Ginoong' : 'Binibining'}</div>
-       
+          <div>Kamusta {foundInvited.gender === 'male' ? 'Ginoong' : 'Binibining'}</div>
+
           <div className="font-semibold text-[#745129]">{foundInvited.first_name} {foundInvited.last_name}</div>
-       
+
           <div className="mt-5">Ikaw po ba ay Makakadalo sa Kasal namin?</div>
         </div>
 
         <div className="flex justify-center items-center gap-5">
-          <button className="py-2 px-7 bg-transparent border-2 border-gray-500 rounded-md text-gray-600 hover:bg-[#db9b51] hover:text-white hover:border-none hover:shadow-md">
+          <button
+            onClick={attending}
+            className={btnStatus === `pending` || btnStatus === `not attending`
+              ? `py-2 px-7 bg-transparent border-2 border-gray-500 rounded-md text-gray-600 hover:bg-[#db9b51] hover:text-white hover:border-none hover:shadow-md`
+              : `py-2 px-7  bg-[#db9b51] text-white shadow-md rounded-md  hover:shadow-none `
+            }
+
+          >
             Makakadalo
           </button>
-          <button className="py-2 px-7 bg-transparent border-2 border-gray-500 rounded-md text-gray-600 hover:bg-[#db9b51] hover:text-white hover:border-none hover:shadow-md">
+
+          <button
+            onClick={notAttending}
+            className={btnStatus === `pending` || btnStatus === `attending`
+              ? `py-2 px-7 bg-transparent border-2 border-gray-500 rounded-md text-gray-600 hover:bg-[#db9b51] hover:text-white hover:border-none hover:shadow-md`
+              : `py-2 px-7  bg-[#db9b51] text-white shadow-md rounded-md  hover:shadow-none `
+            }
+
+          >
             Hindi
           </button>
-          
+
         </div>
 
 
--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,25 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKzeqW4ynCOt93Lyc3Irw5lYa9rncLLXE",
-  authDomain: "wedding-site-a0553.firebaseapp.com",
-  projectId: "wedding-site-a0553",
-  storageBucket: "wedding-site-a0553.appspot.com",
-  messagingSenderId: "194651427996",
-  appId: "1:194651427996:web:669bb418883b6ccca24959"
+  apiKey: "AIzaSyDn7LIMxrSRLgiOibOlKGxkbgMNcrZbiWg",
+  authDomain: "wedding-67895.firebaseapp.com",
+  projectId: "wedding-67895",
+  storageBucket: "wedding-67895.appspot.com",
+  messagingSenderId: "927473408236",
+  appId: "1:927473408236:web:2638c0e580b392d5baedc9"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
+
+
+
+
+// apiKey: "AIzaSyBKzeqW4ynCOt93Lyc3Irw5lYa9rncLLXE",
+// authDomain: "wedding-site-a0553.firebaseapp.com",
+// projectId: "wedding-site-a0553",
+// storageBucket: "wedding-site-a0553.appspot.com",
+// messagingSenderId: "194651427996",
+// appId: "1:194651427996:web:669bb418883b6ccca24959"
